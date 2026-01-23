70cca64962558e5a1549ebb80bbdce5278f3e66f
Vida <quiniavidamarie30@gmail.com>
2026-01-23 23:09:21 +0800

    Reworked the data and rendered all of the drinks from the object to the product page

--- shopping-cart/src/routes/Products.jsx
+++ shopping-cart/src/routes/Products.jsx
@@ -1,7 +1,46 @@
+import { setMenuData } from "./data/MenuData";
+
 export function Products() {
   return (
     <>
-      <div className="products-list"></div>
+      <div className="products-list">
+        <GetData />
+      </div>
     </>
+  );
+}
+
+function GetData() {
+  const { menu, addons } = setMenuData();
+  console.log(menu);
+  return (
+    <>
+      {menu.map((drinks, index) => (
+        <RenderCategory drinks={drinks} key={index} />
+      ))}
+    </>
+  );
+}
+
+function RenderCategory({ drinks }) {
+  return (
+    <div className="drinks-category">
+      <h3>{drinks.category}</h3>
+      {drinks.items.map((drink) => (
+        <RenderDrinks drink={drink} key={drink.id} />
+      ))}
+    </div>
+  );
+}
+
+function RenderDrinks({ drink }) {
+  return (
+    <div className="drinks-details">
+      <div className="drinks-image"></div>
+
+      <div className="drinks-info">
+        <h4>{drink.name}</h4>
+      </div>
+    </div>
   );
 }
