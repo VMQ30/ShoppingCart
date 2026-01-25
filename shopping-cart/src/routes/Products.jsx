82bc570799a2ebbf823aad5636852c88d1c59d4e
Vida <quiniavidamarie30@gmail.com>
2026-01-25 15:44:08 +0800

    started styling the products page

--- shopping-cart/src/routes/Products.jsx
+++ shopping-cart/src/routes/Products.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { setMenuData } from "./data/MenuData";
 import { NavLink, useLoaderData } from "react-router-dom";
 
+import styles from "./styles/Products.module.css";
+
 export function ProductsLoader({ params }) {
   const { category } = params;
   const finalCategory = decodeURIComponent(category);
@@ -21,7 +23,7 @@ export function ProductsLoader({ params }) {
 export function Products() {
   return (
     <>
-      <div className="products-list">
+      <div className={styles["products-list"]}>
         <GetData />
       </div>
     </>
@@ -33,93 +35,104 @@ function GetData() {
   const { categoryData } = useLoaderData();
   const drinks = categoryData.items;
 
-  console.log(categoryData);
   return (
-    <>
-      <div className="drinks-category">
+    <div className={styles["drinks-container"]}>
+      <div className={styles["drinks-category"]}>
         {menu.map((drinks, index) => (
           <RenderCategory drinks={drinks} key={index} />
         ))}
       </div>
 
-      <div className="drinks-list">
+      <div className={styles["drink-list-wrapper"]}>
         <h2>{categoryData.category}</h2>
-        {drinks.map((drink) => (
-          <RenderDrinks drink={drink} key={drink.id} />
-        ))}
+
+        <div className={styles["drinks-list"]}>
+          {drinks.map((drink) => (
+            <RenderDrinks drink={drink} addons={addons} key={drink.id} />
+          ))}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
 function RenderCategory({ drinks }) {
   return (
-    <div className="drinks-category">
+    <>
       <h3 key={drinks.category}>
         <NavLink to={`/products/${drinks.category}`}>{drinks.category}</NavLink>
       </h3>
-    </div>
-  );
-}
-
-function RenderDrinks({ drink }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  return (
-    <>
-      <div className="drinks-card" onClick={() => setIsModalOpen(!isModalOpen)}>
-        <div className="drinks-details">
-          <div className="drinks-info">
-            <div>
-              <h4>{drink.name}</h4>
-              {drink.bestSeller && (
-                <div className="best-seller">Best Seller </div>
-              )}
-            </div>
-            <p>{drink.description}</p>
-          </div>
-          <h6>
-            {drink.size.map(
-              (size) => `${Object.keys(size)} : ₱${Object.values(size)} `,
-            )}
-          </h6>
-        </div>
-        <div className="drinks-image"></div>
-      </div>
-
-      {isModalOpen && <Modal drink={drink} />}
     </>
   );
 }
 
-function Modal({ drink }) {
+function RenderDrinks({ drink, addons }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <div
+        className={styles["drinks-card"]}
+        onClick={() => setIsModalOpen(!isModalOpen)}
+      >
+        <div className={styles["drinks-details"]}>
+          <div className={styles["drinks-info"]}>
+            <div className={styles["drinks-image"]}></div>
+            <div className={styles["drinks-name"]}>
+              <h4>{drink.name}</h4>
+              {drink.bestSeller && (
+                <div className={styles["best-seller"]}>
+                  <p>Best Seller</p>
+                </div>
+              )}
+            </div>
+            <p>{drink.description}</p>
+          </div>
+          <div className={styles["drinks-price"]}>
+            <h6>
+              {drink.size.map(
+                (size) => `${Object.keys(size)} : ₱${Object.values(size)} `,
+              )}
+            </h6>
+          </div>
+        </div>
+      </div>
+
+      {isModalOpen && <Modal drink={drink} addons={addons} />}
+    </>
+  );
+}
+
+function Modal({ drink, addons }) {
   const [numOfOrder, setNumOfOrder] = useState(1);
+  const [selectedSize, setSelectedSize] = useState(null);
+
   return (
     <div
-      className="modal"
+      className={styles.modal}
       role="dialog"
       aria-modal="true"
       aria-labelledby="modal-title"
     >
       <div
-        className="modal-photo"
+        className={styles["modal-photo"]}
         role="img"
         aria-label={`Photo of ${drink.name}`}
       ></div>
 
-      <div className="drink-details">
+      <div className={styles["drink-details"]}>
         <h3 id="modal-title">{drink.name}</h3>
         <p>{drink.description}</p>
       </div>
 
-      <div className="drink-option">
-        <fieldset className="size-selector-field">
+      <div className={styles["drink-option"]}>
+        <fieldset className={styles["size-selector-field"]}>
           <legend>
-            <h4 id="size-label">Size: </h4>
+            <h4 id="size-label">* Size: </h4>
           </legend>
 
           <div
-            className="options-wrapper"
+            className={styles["options-wrapper"]}
             role="radiogroup"
             aria-labelledby="size-label"
           >
@@ -129,12 +142,13 @@ function Modal({ drink }) {
               const drinkId = `${drink.id}-${drinkSize}`;
 
               return (
-                <div className="option-size" key={drinkId}>
+                <div className={styles["option-size"]} key={drinkId}>
                   <input
                     id={drinkId}
                     type="radio"
                     name="drinksSize"
                     value={drinkSize}
+                    onChange={() => setSelectedSize(size)}
                   />
                   <label htmlFor={drinkId}>{drinkSize}</label>
                   <h6>{`₱${drinkPrice}.00`}</h6>
@@ -145,28 +159,56 @@ function Modal({ drink }) {
         </fieldset>
       </div>
 
-      <div className="drink-request">
+      <div className={styles["drink-add-ons"]}>
+        <fieldset className={styles["add-ons-selector-field"]}>
+          <legend>
+            <h4 id="add-ons-label">Add-Ons: </h4>
+          </legend>
+
+          <div
+            className={styles["options-wrapper"]}
+            aria-labelledby="add-ons-label"
+          >
+            {addons.map((addon) => (
+              <div className={styles["option-size"]} key={addon.id}>
+                <input
+                  id={addon.id}
+                  type="checkbox"
+                  name="addons"
+                  value={addon.name}
+                />
+                <label htmlFor={addon.id}>{addon.name}</label>
+                <h6>{`₱${addon.price}.00`}</h6>
+              </div>
+            ))}
+          </div>
+        </fieldset>
+      </div>
+
+      <div className={styles["drink-request"]}>
         <h4>Special Request</h4>
         <textarea />
       </div>
 
-      <div className="add-order">
-        <div className="num-of-order-wrapper">
+      <div className={styles["add-order"]}>
+        <div className={styles["num-of-order-wrapper"]}>
           <button
-            className="decrease"
+            className={styles.decrease}
             onClick={() => setNumOfOrder((prev) => (prev > 1 ? prev - 1 : 1))}
           >
             -
           </button>
-          <div className="num-of-order">{numOfOrder}</div>
+          <div className={styles["num-of-order"]}>{numOfOrder}</div>
           <button
-            className="increase"
+            className={styles.increase}
             onClick={() => setNumOfOrder((prev) => prev + 1)}
           >
             +
           </button>
         </div>
-        <button className="add-cart">Add to Cart</button>
+        <button className={styles["add-cart"]} disabled={!selectedSize}>
+          Add to Cart
+        </button>
       </div>
     </div>
   );
