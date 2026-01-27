59768ab32570e9845c44c9c04c9aef2c78867068
Vida <quiniavidamarie30@gmail.com>
2026-01-27 23:21:15 +0800

    Finished styling the hero section

--- shopping-cart/src/routes/Home.jsx
+++ shopping-cart/src/routes/Home.jsx
@@ -4,11 +4,12 @@ import sakuraGIF from "../assets/sakura.gif";
 import milkSplash from "../assets/milk-splash.png";
 import milkTea from "../assets/milktea.png";
 
-import splash from "../assets/splash.png";
 import leaf1 from "../assets/leaf1.png";
 import leaf2 from "../assets/leaf2.png";
 import leaf3 from "../assets/leaf3.png";
 import pearl from "../assets/pearl.png";
+import strawberrySlice from "../assets/strawberryslice.png";
+import strawberry from "../assets/strawberry.png";
 
 export function Home() {
   return (
@@ -18,13 +19,20 @@ export function Home() {
           <img src={milkSplash} className={styles["milk-splash"]} />
           <img src={milkTea} className={styles["milk-tea"]} />
 
-          <img src={splash} className={styles["splash"]} />
+          <img src={leaf1} className={styles["leaf1"]} />
+          <img src={leaf2} className={styles["leaf2"]} />
+          <img src={pearl} className={styles["pearl"]} />
+          <img src={pearl} className={styles["pearl2"]} />
+          <img src={pearl} className={styles["pearl3"]} />
+          <img src={strawberrySlice} className={styles["strawberry-slice"]} />
+          <img src={strawberry} className={styles["strawberry"]} />
+          {/* <img src={splash} className={styles["splash"]} /> */}
         </div>
 
         <div className={styles["hero-section-right"]}>
           <div className={styles["hero-section-right-wrapper"]}>
-            <img src={sakuraGIF} className={styles["sakura-one"]}></img>
-            <img src={sakuraGIF} className={styles["sakura-two"]}></img>
+            {/* <img src={sakuraGIF} className={styles["sakura-one"]}></img>
+            <img src={sakuraGIF} className={styles["sakura-two"]}></img> */}
 
             <p>A taste of spring in every drop</p>
             <NavLink to="products/Milk Tea">View Products</NavLink>
